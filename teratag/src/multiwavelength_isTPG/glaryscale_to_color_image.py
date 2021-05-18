ac866a635c63bcb79cbb1618cdff3b9cd6764a9a
yt0502 <yt050297@gmail.com>
2021-05-18 15:19:17 +0900

    fixed grayscale to color image

--- teratag/src/multiwavelength_isTPG/glaryscale_to_color_image.py
+++ teratag/src/multiwavelength_isTPG/glaryscale_to_color_image.py
@@ -5,9 +5,9 @@ import os
 import glob
 
 #画像のディレクトリ指定
-dir_path = '/*'
+dir_path = 'C:/Users/yt050/Desktop/grayscale_pic/*.png'
 #保存するディレクトリを指定
-save_path = ''
+save_path = 'C:/Users/yt050/Desktop/grayscale_pic/converted_to_color_image'
 os.makedirs(save_path, exist_ok=True)
 
 
@@ -18,9 +18,9 @@ for i, file in enumerate(file_list):
         pic=cv2.imread(file, cv2.IMREAD_GRAYSCALE)
         print('実行中のファイル名 : {}'.format(file))
         save_filename1 = os.path.splitext(os.path.basename(file))[0]
-        save_filename2 = save_filename1 + '_after_math.png'
+        save_filename2 = save_filename1 + '_converted_to_color.png'
         save_filename = os.path.join(save_path, save_filename2)
-        print('保存先のファイル名 : {}'.format(save_filename))
+        #print('保存先のファイル名 : {}'.format(save_filename))
 
         #疑似カラー化_JET
         pseudo_color = cv2.applyColorMap(pic, cv2.COLORMAP_JET)
@@ -35,4 +35,11 @@ for i, file in enumerate(file_list):
         #疑似カラー化_RAINBOW
         pseudo_color = cv2.applyColorMap(pic, cv2.COLORMAP_RAINBOW)
         '''
+        #画像のサイズ変更、表示のため
+        height = pseudo_color.shape[0]
+        width = pseudo_color.shape[1]
+        resized_img = cv2.resize(pseudo_color,(width//4,height//4))
+        cv2.imshow(save_filename2, resized_img)
+        cv2.waitKey(0)
+        #画像の保存
         cv2.imwrite(save_filename, np.array(pseudo_color))
