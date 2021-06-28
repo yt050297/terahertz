8d7b559f184e664e76bffc4cfb21ba801280cf6a
yt0502 <yt050297@gmail.com>
2021-06-28 15:40:33 +0900

    fixed imaging program

--- teratag/src/multiwavelength_isTPG/stage_and_imaging_ver2.py
+++ teratag/src/multiwavelength_isTPG/stage_and_imaging_ver2.py
@@ -10,13 +10,16 @@ def main():
     print('ok')
     port = 'COM3'
     list = []
-    side_pixel = 5
-    height_pixel = 5
-    side_stage=-10000
-    height_stage=-10000
+    side_pixel = 20
+    height_pixel = 20
+    side_stage=-10000   ###4um/pulse
+    height_stage=-10000   ###1um/pulse
+    side_resolution = 250
+    height_resolution = 1000
     spd_min = 19000  # 最小速度[PPS]
     spd_max = 20000  # 最大速度[PPS]
     acceleration_time = 1000  # 加減速時間[mS]
+    sec=0.2
 
     X = np.zeros((height_pixel, side_pixel))
 
@@ -28,22 +31,22 @@ def main():
 
     print("Reset")
     polarizer.reset()
-    time.sleep(5)
+    time.sleep(8)
     print('初期位置設定')
     polarizer._set_position_relative(1, side_stage)
-    time.sleep(5)
+    time.sleep(6)
     polarizer._set_position_relative(2, height_stage)
-    time.sleep(5)
+    time.sleep(6)
     #print(polarizer._get_position())
     #list.append(polarizer._get_position())
     #print(list)
 
 
     ##shoki
-    polarizer._set_position_relative(2, -500)
-    time.sleep(0.6)
+    polarizer._set_position_relative(2, -height_resolution)
+    time.sleep(sec)
     # print(polarizer._get_position())
-    list.append(polarizer._get_position())
+    #list.append(polarizer._get_position())
     # print(list)
     # print(len(list))  # 全ピクセル数＋１になっている
     i=j=0
@@ -58,18 +61,18 @@ def main():
     for i in range(height_pixel):
         for j in range(1,side_pixel):
             if i % 2 == 0:
-                polarizer._set_position_relative(1, -500)  # 引数一つ目、1:一軸、2:2軸、W:両軸
-                time.sleep(0.6)
+                polarizer._set_position_relative(1, -side_resolution)  # 引数一つ目、1:一軸、2:2軸、W:両軸
+                time.sleep(sec)
                 a = 1
                 X[i][j] = a
                 print(i, j)
             else:
-                polarizer._set_position_relative(1, 500)  # 引数一つ目、1:一軸、2:2軸、W:両軸
-                time.sleep(0.6)
+                polarizer._set_position_relative(1, side_resolution)  # 引数一つ目、1:一軸、2:2軸、W:両軸
+                time.sleep(sec)
                 # a = np.random.randint(0, 2)
                 a = 2
                 X[i][side_pixel-1-j] = a
-                print(i, height_pixel-1-j)
+                print(i, side_pixel-1-j)
             #list.append(polarizer._get_position())
             # print(list)
             # print(len(list))   #全ピクセル数＋１になっている
@@ -79,8 +82,8 @@ def main():
             plt.title("Plot 2D array")
             plt.show()
 
-        polarizer._set_position_relative(2, -500)
-        time.sleep(0.6)
+        polarizer._set_position_relative(2, -height_resolution)
+        time.sleep(sec)
         # print(polarizer._get_position())
         #list.append(polarizer._get_position())
         #print(list)
@@ -91,18 +94,16 @@ def main():
             a = 3
             X[i+1][j] = a
             print(i+1,j)
-            fig = plt.figure(figsize=(5, 5))
-            plt.imshow(X, cmap='bwr')
-            plt.title("Plot 2D array")
-            plt.show()
         else:
             a = 3
-            X[i+1][j-4] = a
-            print(i+1, j-4)
-            fig = plt.figure(figsize=(5, 5))
-            plt.imshow(X, cmap='bwr')
-            plt.title("Plot 2D array")
-            plt.show()
+            X[i+1][side_pixel-1-j] = a
+            print(i+1, side_pixel-1-j)
+        fig = plt.figure(figsize=(5, 5))
+        plt.imshow(X, cmap='bwr')
+        plt.title("Plot 2D array")
+        plt.colorbar()
+        plt.show()
+        plt.savefig('C:/Users/yt050/Desktop/saveimaging/{}_{}.jpg'.format(i,j))
 
     time.sleep(1)
     polarizer.stop()
